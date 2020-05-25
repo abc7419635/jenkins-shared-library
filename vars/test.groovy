9fb69461034aa625a48cca87908b39473d6522e8
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-25 15:41:21 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -24,6 +24,7 @@ def call(body) {
                     label 'ServerModelBuildPC'
                 }
                 steps {
+                    echo ${params.GameModelPath}
                     dir(${params.GameModelPath}) {
                         //checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_DailyCCB', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/RD_DailyCCB', type: 'WRITABLE', view: '')))
                         checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_GameModel', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/GameModel', type: 'WRITABLE', view: '')))
