f457be03daf8482592a3eb5160794f96ee883ec6
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-25 23:55:59 +0800

    Update GameModelServiceScript.groovy

--- vars/GameModelServiceScript.groovy
+++ vars/GameModelServiceScript.groovy
@@ -37,6 +37,7 @@ def call(body) {
         stage('Sync Perforce') {
             dir(env.P4RootDir) {
                 bat 'exit 1'
+                bat 'exit 0'
                 //checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_DailyCCB', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/RD_DailyCCB', type: 'WRITABLE', view: '')))
                 //checkout perforce(credential: env.P4Credential, populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: env.P4Workspace, pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: env.P4Stream, type: 'WRITABLE', view: '')))
             }
