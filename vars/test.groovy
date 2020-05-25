a9a2eff1f87d6dadd09290c7a14b53bf971cd058
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-25 17:42:05 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -8,7 +8,7 @@ def call(body) {
                 //choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
                 //${params.CHOICE}
                 //password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-                booleanParam(name: 'Refresh', defaultValue: true, description: '')
+                booleanParam(name: 'Refresh', defaultValue: false, description: '')
                 string(name: 'GameModelPath', defaultValue: 'D:\\RD_GameModel', description: 'GameModel Path')
                 booleanParam(name: 'Deploy', defaultValue: false, description: 'deploy to staging')
         }
@@ -19,88 +19,76 @@ def call(body) {
                     environment name: 'Refresh', value: 'true'
                 }
                 steps {
-                    echo("Ended pipeline early.")     
+                    error("Ended pipeline early.")     
                 }
             }
-        
-            stage('Run Jenkinsfile') {
-                when {
-                    environment name: 'Refresh', value: 'false'
-                }
-                stage('Build') {
-                    echo "Build"
-                }
-                stage('Test') {
-                    echo "Test"
-                }
-                stage('Deploy') {
-                    echo "Deploy"
-                }
-            }
-            /*stage('Run Jenkinsfile') {
-                when {
-                    environment name: 'Refresh', value: 'false'
-                }
 
+            stage('Sync Perforce') {
                 agent {
                     label 'ServerModelBuildPC'
                 }
-
-                stage('Sync Perforce') {
-                    steps {
-                        dir("${params.GameModelPath}") {
-                            //checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_DailyCCB', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/RD_DailyCCB', type: 'WRITABLE', view: '')))
-                            checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_GameModel', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/GameModel', type: 'WRITABLE', view: '')))
-                        }
+                steps {
+                    dir("${params.GameModelPath}") {
+                        //checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_DailyCCB', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/RD_DailyCCB', type: 'WRITABLE', view: '')))
+                        checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_GameModel', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/GameModel', type: 'WRITABLE', view: '')))
                     }
                 }
+            }
 
-                stage('Build Services') {
-                    steps {
-                        dir("${params.GameModelPath}") {
-                            bat '''
-                            cd GameModel\\Model.Server
-                            call Deployment\\Bot\\ClearLogs.bat
-                            call Deployment\\DeployCore\\Instances\\ClearLogs.bat
-                            "..\\Tools\\ExcelParser\\MSBuild\\15.0\\Bin\\MSBuild.exe" "GameModel.sln" -p:Configuration=Release -restore -t:rebuild
-                            '''
-                        }
-                    }
+            stage('Build Services') {
+                agent {
+                    label 'ServerModelBuildPC'
                 }
-
-                stage('CompressUpload') {
-                    steps {
+                steps {
+                    dir("${params.GameModelPath}") {
                         bat '''
-                            set x=%date:~0,4%%date:~5,2%%date:~8,2%
-                            IF "%time:~0,1%" == " " (
-                                set y=0%time:~1,1%%time:~3,2%%time:~6,2%
-                            )ELSE (
-                                set y=%time:~0,2%%time:~3,2%%time:~6,2%
-                            )
-
-                            7z a %GameModelPath%\\GameModel\\DeploymentPack\\Release_%BUILD_NUMBER%_%x%_%y% %GameModelPath%\\GameModel\\Model.Server\\Deployment
-
-                            set BOTO_CONFIG=D:\\JenkinsRemoteRoot\\.boto
-                            gsutil cp %GameModelPath%\\GameModel\\DeploymentPack\\Release_%BUILD_NUMBER%_%x%_%y%.7z gs://server_model_release/
-                            '''
+                        cd GameModel\\Model.Server
+                        call Deployment\\Bot\\ClearLogs.bat
+                        call Deployment\\DeployCore\\Instances\\ClearLogs.bat
+                        "..\\Tools\\ExcelParser\\MSBuild\\15.0\\Bin\\MSBuild.exe" "GameModel.sln" -p:Configuration=Release -restore -t:rebuild
+                        '''
                     }
                 }
-                stage('DeployToStaging') {
-                    when {
-                        environment name: 'Deploy', value: 'true'
-                    }
-                    steps {
-                        bat '''
-                            set TAR_PATH=D:\\Docker\\service\\ansible\\volume\\ansible\\Deployment.tar
-                            del %TAR_PATH%&
-                            call 7z a -ttar %TAR_PATH% %GameModelPath%\\Model.Server\\Deployment
-                            docker exec ansible ansible-playbook ./ansible/playbooks/staging/update-services.yml
-                            docker exec ansible ansible-playbook ./ansible/playbooks/staging/start-services.yml -vvv
-                            python D:\\_Pythan\\ReportSuccess.py StagingServerStart
-                            '''
-                    }
+            }
+
+            stage('CompressUpload') {
+                agent {
+                    label 'ServerModelBuildPC'
                 }
-            }*/
+                steps {
+                    bat '''
+                        set x=%date:~0,4%%date:~5,2%%date:~8,2%
+                        IF "%time:~0,1%" == " " (
+                            set y=0%time:~1,1%%time:~3,2%%time:~6,2%
+                        )ELSE (
+                            set y=%time:~0,2%%time:~3,2%%time:~6,2%
+                        )
+
+                        7z a %GameModelPath%\\GameModel\\DeploymentPack\\Release_%BUILD_NUMBER%_%x%_%y% %GameModelPath%\\GameModel\\Model.Server\\Deployment
+
+                        set BOTO_CONFIG=D:\\JenkinsRemoteRoot\\.boto
+                        gsutil cp %GameModelPath%\\GameModel\\DeploymentPack\\Release_%BUILD_NUMBER%_%x%_%y%.7z gs://server_model_release/
+                        '''
+                }
+            }
+            stage('DeployToStaging') {
+                when {
+                    environment name: 'Deploy', value: 'true'
+                }
+                agent {
+                    label 'ServerModelBuildPC'
+                }
+                steps {
+                    bat '''
+                        set TAR_PATH=D:\\Docker\\service\\ansible\\volume\\ansible\\Deployment.tar
+                        del %TAR_PATH%&
+                        call 7z a -ttar %TAR_PATH% %GameModelPath%\\Model.Server\\Deployment
+                        docker exec ansible ansible-playbook ./ansible/playbooks/staging/update-services.yml
+                        docker exec ansible ansible-playbook ./ansible/playbooks/staging/start-services.yml -vvv
+                        python D:\\_Pythan\\ReportSuccess.py StagingServerStart
+                        '''
+                }
+            }
         }
     }
 }
