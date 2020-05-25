d3a2586606e3e9e389a6463fbc074c744dc6a232
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-26 00:06:35 +0800

    Update GameModelServiceScript.groovy

--- vars/GameModelServiceScript.groovy
+++ vars/GameModelServiceScript.groovy
@@ -44,12 +44,14 @@ def call(body) {
         stage('Build Services') {
             dir(env.P4RootDir) {
                 bat '''
-                set AABBCC=AABBCC
                 cd GameModel\\Model.Server
                 call Deployment\\Bot\\ClearLogs.bat
                 call Deployment\\DeployCore\\Instances\\ClearLogs.bat
                 "..\\Tools\\ExcelParser\\MSBuild\\15.0\\Bin\\MSBuild.exe" "GameModel.sln" -p:Configuration=Release -restore -t:rebuild
                 '''
+            }
+            script{
+                env.AABBCC='AABBCC'
             }
         }
 
