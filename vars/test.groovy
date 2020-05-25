b543be2e8d09742b8c31e1d49feceb710102369e
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-25 11:40:38 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -16,7 +16,7 @@ def call(body) {
         stages {
             stage('Example') {
                 steps {
-                    echo "${body}"
+                    checkout perforce(credential: 'programmer', populate: syncOnly(force: false, have: true, modtime: false, quiet: false, revert: false), workspace: manualSpec(charset: 'utf8', name: 'RD_DailyCCB', pinHost: true, spec: clientSpec(allwrite: false, backup: true, changeView: '', clobber: true, compress: false, line: 'UNIX', locked: false, modtime: false, rmdir: true, serverID: '', streamName: '//GD2ReDream/DailyCCB', type: 'WRITABLE', view: '')))
                 }
             }
         }
