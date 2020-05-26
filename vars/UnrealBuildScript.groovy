1b79dbd62fa1641ec5e3b3f53780b67c35863070
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-26 17:00:31 +0800

    Update UnrealBuildScript.groovy

--- vars/UnrealBuildScript.groovy
+++ vars/UnrealBuildScript.groovy
@@ -11,7 +11,12 @@ pipeline {
         choice(name: 'P4Stream', choices: ['//GD2ReDream/DailyBuild'], description: '')
         string(name: 'P4RootDir', defaultValue: 'D:\\RD_DailyBuild', description: '')
         string(name: 'BOTO_CONFIG', defaultValue: 'D:\\JenkinsRemoteRoot\\.boto', description: '')
-        string(name: 'GSPath', defaultValue: 'gs://server_model_release/', description: '')
+
+        string(name: 'UNREAL_BUILD_DIR', defaultValue: 'E:\\ReDreamPackage', description: '')
+        string(name: 'UNREAL_GAME_DIR', defaultValue: 'D:\\RD_DailyBuild\\Game\\ReDream\\ReDream.uproject', description: '')
+        string(name: 'UNREAL_SOURCECODE_DIR', defaultValue: 'D:\\RD_DailyBuild\\Game', description: '')
+
+        booleanParam(name: 'CLEARCOOK', defaultValue: false, description: '')
     }
     
     stages {
@@ -32,7 +37,7 @@ if(env.Refresh=='false')
 import java.text.SimpleDateFormat
 
 def call(body) {
-    node('ServerModelBuildPC') {
+    node('RemoteBuildPC') {
         stage('Sync Perforce') {
             if(env.SkipP4Update=='false')
             {
@@ -48,6 +53,30 @@ def call(body) {
             {
                 echo 'Skip Sync Perforce'
             }
+        }
+
+        stage('PreBuild') {
+            bat '''
+                rmdir /s/q %UNREAL_BUILD_DIR%
+                md %UNREAL_BUILD_DIR%
+
+                echo %P4_CHANGELIST% > "D:\\_BuildTools\\temp\\ContentVersion.txt"
+
+                set P4USER=programmer
+                set P4PASSWD=F5E023356D8072F0A3521F83E5678CE0
+                set P4PORT=10.2.100.220:1001
+                set P4CLIENT=RD_DailyBuild
+
+                for /F %%i IN (D:\\_BuildTools\\temp\\ContentVersion.txt) DO (
+                set ContentVersion=%%i
+                )
+
+                p4 edit -c default d:\\RD_DailyBuild\\Game\\ReDream\\Config\\DefaultGame.ini
+                %UNREAL_SOURCECODE_DIR%\\Engine\\Binaries\\ThirdParty\\Python\\Win64\\python.exe D:\\_BuildTools\\Python\\configReaderV2.py %BUILD_ID% %ContentVersion% %SourceVersion%
+                p4 submit -d "[AutoBuild] Auto Increase Version %BUILD_ID% ContentVer:%ContentVersion%" -f revertunchanged
+
+                echo %BUILD_ID% > "D:\\_BuildTools\\temp\\BuildVersion.txt"
+                '''
         }
     }
 }
