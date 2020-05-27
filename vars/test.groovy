f2801c43be29170dfa98c88ad0af919ded490725
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-27 21:00:52 +0800

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,6 +3,8 @@ import java.text.SimpleDateFormat
 def call(body) {
     node('RemoteBuildPC') {        
         stage('test') {
+            ​TimeZone.setDefault(TimeZone.getTimeZone('CST'))
+            
             def date = new Date()
             def sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
             def timestring = sdf.format(date)
