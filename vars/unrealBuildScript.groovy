c13da81e0ba4a8bbc3246c0a007cb86374350c3c
Hao <43645684+abc7419635@users.noreply.github.com>
2020-05-28 14:58:23 +0800

    Update unrealBuildScript.groovy

--- vars/unrealBuildScript.groovy
+++ vars/unrealBuildScript.groovy
@@ -90,17 +90,18 @@ def call(body) {
 
                     echo %P4_CHANGELIST% > "D:\\_BuildTools\\temp\\ContentVersion.txt"
 
-                    set P4USER=programmer
-                    set P4PASSWD=F5E023356D8072F0A3521F83E5678CE0
-                    set P4PORT=10.2.100.220:1001
-                    set P4CLIENT=%P4WorkspaceName%
+                    set P4USER=%P4_USER%
+                    set P4PASSWD=%P4_TICKET%
+                    set P4PORT=%P4_PORT%
+                    set P4CLIENT=%P4_CLIENT%
 
                     for /F %%i IN (D:\\_BuildTools\\temp\\ContentVersion.txt) DO (
                     set ContentVersion=%%i
                     )
 
-                    p4 edit -c default %P4RootDir%\\Game\\ReDream\\Config\\DefaultGame.ini
-                    %UNREAL_SOURCECODE_DIR%\\Engine\\Binaries\\ThirdParty\\Python\\Win64\\python.exe D:\\_BuildTools\\Python\\configReaderV2.py %BUILD_ID% %ContentVersion%
+                    set CheckOutFile=%P4_ROOT%\\Game\\ReDream\\Config\\DefaultGame.ini
+                    p4 edit -c default %CheckOutFile%
+                    %UNREAL_SOURCECODE_DIR%\\Engine\\Binaries\\ThirdParty\\Python\\Win64\\python.exe D:\\_BuildTools\\Python\\configReaderV2.py %BUILD_ID% %ContentVersion% %CheckOutFile%
                     p4 submit -d "[AutoBuild] Auto Increase Version %BUILD_ID% ContentVer:%ContentVersion%" -f revertunchanged
 
                     echo %BUILD_ID% > "D:\\_BuildTools\\temp\\BuildVersion.txt"
